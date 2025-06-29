2955991e1a63f30ce5d6f37ec7b1be41133cc720
Blaze <xblazek1ng420x@gmail.com>
2025-06-29 01:52:36 -0700

    Update themer.js

--- assets/BlazeCord/Bundle/Test/themer.js
+++ assets/BlazeCord/Bundle/Test/themer.js
@@ -20895,7 +20895,7 @@ ${inspect({
       "use strict";
       init_registry();
       init_objects();
-      // Object.freeze = Object.seal = Object;
+      Object.freeze = Object.seal = Object;
       unhook = hookDefineProperty(globalThis, "__d", (define2) => {
         unhook();
         globalThis.__d = internal_getDefiner(define2, (runFactory) => {
